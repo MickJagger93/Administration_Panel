8f794f159b62cd291a905e6d46444495c68f3232
JaggerMQ <quevedo993mi@gmail.com>
2026-04-04 03:51:46 +0530

    deploy

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from models import db, Admin
 from flask_migrate import Migrate
 from config import DevelopmentConfig
 from flask_wtf.csrf import CSRFProtect
+import os
 
 def create_app():
 
@@ -37,4 +38,5 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
